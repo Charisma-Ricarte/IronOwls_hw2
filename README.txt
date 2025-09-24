cb9661461a83ce40be19eada2623f2aff348cebd
Charisma-Ricarte <ann.marie.chr29@gmail.com>
2025-09-23 23:42:42 -0500

    Update

--- README.txt
+++ README.txt
@@ -4,6 +4,7 @@ User-Agent: http_client/1.0\r\n
 Connection: close\r\n
 \r\n
 
+python http_client.py -u https://zechuncao.com/teaching/csci4406/testfiles/test1.txt -o test1.txt
 
 chmod +x http_client.py
 
@@ -12,11 +13,18 @@ Questions
 1. What is the difference between http_client (what you wrote), curl, and wget? Use those tools 
 and read about them online before you answer this question.
 
+Ours shows how to download files through sending HTTP/1.0 requests through a socket. 
+Ours only supports http:// downloads unlike curl and wget. It would
+not be able to handle redirects for any kind of advanced type of feature, this in turn 
+would make ours more limited. 
+
 
 2. Some websites allow only certain browsers (e.g., Firefox) to download content. When an HTTP 
 request comes in, those servers check what browser is sending the request. How can you write 
 http_client so it is able to download that content despite that check? 
-
+User-Agent can be added in the string to mimick the browser in order to bypass the check. 
+Example. The new change will look like this.
+f"GET {path} HTTP/1.0\r\nHost: {host}\r\n\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0\r\n"
 
 3. If you were running such a website that needed to restrict its content to a certain browser, 
 how can you prevent programs like http_client from downloading the content from your site?
--- http_client.py
+++ http_client.py
@@ -3,21 +3,34 @@ import socket
 import argparse
 from urllib.parse import urlparse
 import os 
+import ssl
 
 def download(url, output_file=None):
     parsed_url = urlparse(url)
 
     host = parsed_url.hostname
-    port = parsed_url.port if parsed_url.port else 80
     path = parsed_url.path if parsed_url.path else "/"
+    port = parsed_url.port
+
+    use_ssl = False
+    if parsed_url.scheme == "https":
+        use_ssl = True
+        port = port if port else 443
+    else:
+        port = port if port else 80
 
     if output_file is None:
         output_file is os.path.basename(path) if os.path.basename(path) else "index.html"
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    if use_ssl:
+        context = ssl.create_default_context()
+        sock = context.wrap_socket(sock, server_hostname=host)
+
     sock.connect((host, port))
 
-    request = (f"GET {path} HTTP/1.0\r\nHost: {host}\r\n\r\n")
+    request = f"GET {path} HTTP/1.0\r\nHost: {host}\r\n\r\n"
     sock.sendall(request.encode())
 
     response = b""
@@ -31,9 +44,7 @@ def download(url, output_file=None):
     header_end = response.find(b"\r\n\r\n")
     if header_end == -1:
         raise Exception("Invalid HTTP response: no header-body separtor found")
-    
-    header = response[:header_end].decode(errors="ignore")
-    body = response[header_end+4] # skip the \r\n\r\n
+    body = response[header_end+4:]
 
     with open(output_file, "wb") as f:
         f.write(body)
@@ -46,7 +57,7 @@ def main():
     parser.add_argument("-o", "--output", required=True, help="Output file name")
     args = parser.parse_args()
 
-    download(args.url, arg.output)
+    download(args.url, args.output)
 
 if __name__ == "__main__":
     main()
